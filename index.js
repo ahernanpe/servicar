c55e63cf6b9d91d83453d88680a7281a1e299990
Alvaro Jesus Hernandez Peña <alvarohdz@yahoo.com>
2021-02-25 21:06:32 -0600

    mejor

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-/*
+
 const express = require('express');
 const app = express();
+const importData = require("data.json");
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+
+app.get("/services", (req,res) => {
+    res.send(importData)
+})
+
+
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
-*/
+
+
+
+
+
+/*
 const express = require("express");
 const app = express();
-const importData = require("./data.json");
+const importData = require("data.json");
 const port = '8080';
 
 /*
@@ -17,19 +29,3 @@ let port = 8080;
 
 let port = process.env.port ;
 */
-app.set('port',process.env.PORT || 3000);
-
-app.get("/", (req,res) => {
-
-    res.send("Hello World")
-})
-
-app.get("/services", (req,res) => {
-    res.send(importData)
-})
-
-app.listen(port, () => {
-    console.log('Example app is listening ');
-
-})
-
